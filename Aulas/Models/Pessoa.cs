d1dee133c1afa3e9428440a36247037d2bc6a03e
Well-199 <wellington.marunaka@gmail.com>
2024-12-19 15:29:42 -0300

    Adicionar arquivos de projeto.

---
+++ Aulas/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Aulas.Models
+{
+    public class Pessoa
+    {
+        public string? Name { get; set; }
+        public int? Age {  get; set; }
+    }
+}
---
+++ Aulas/Program.cs
@@ -0,0 +1,31 @@
+﻿using Aulas.Models;
+using System.Text.Json;
+
+List<int> numbers = [1, 2, 3, 4, 5, 6];
+
+List<Pessoa> pessoas = [];
+
+foreach (int number in numbers)
+{
+    Pessoa people = new()
+    {
+        Name = "Wellington",
+        Age = number,
+    };
+    pessoas.Add(people);
+
+    if (people.Name.Contains("Wellington"))
+    {
+        Console.WriteLine($"Contem o: { people.Name }");
+    }
+}
+
+if (numbers.Contains(6))
+{
+    Console.WriteLine($"Contem o número: {6}");
+}
+
+pessoas.Clear();
+
+string json = JsonSerializer.Serialize(pessoas);
+Console.WriteLine(json);
